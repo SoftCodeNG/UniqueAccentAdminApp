50904f4e88469fc2b7b0e6bb65bec4f28b490380
Uche Esther <ucheesther87@gmail.com>
2021-04-06 16:52:50 +0100

    worked on admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {CoursesComponent} from './pages/courses/courses.component';
+import {SettingsComponent} from './pages/settings/settings.component';
+import {QuizComponent} from './pages/quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    DashboardComponent,
+    CoursesComponent,
+    SettingsComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
