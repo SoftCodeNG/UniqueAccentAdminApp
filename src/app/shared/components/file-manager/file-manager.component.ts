4ff26e51ab016f6b101ddfd6cb391aa32a5a13c2
Augustine Nwafor <austineforall@gmail.com>
2021-05-07 21:08:42 +0100

    UI Cleanup

--- src/app/shared/components/file-manager/file-manager.component.ts
+++ src/app/shared/components/file-manager/file-manager.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {SetFileManagerState} from "../../../store/app-store/app.action";
-import {Store} from "@ngxs/store";
+import {SetFileManagerState} from '../../../store/app-store/app.action';
+import {Store} from '@ngxs/store';
+import {FileManagerService} from '../../../core/services/file-manager.service';
 
 @Component({
   selector: 'app-file-manager',
@@ -9,12 +10,49 @@ import {Store} from "@ngxs/store";
 })
 export class FileManagerComponent implements OnInit {
   currentView = 'image';
+  allMedia: any[];
+  allImages: any[];
+  allVideo: any[];
+  allAudio: any[];
 
   constructor(
-    private store: Store
+    private store: Store,
+    private fileManagerService: FileManagerService
   ) { }
 
   ngOnInit(): void {
+    this.getAllMedia();
+    this.getAllImages();
+    this.getAllVideo();
+    this.getAllAudio();
+  }
+
+  getAllMedia(): void {
+    this.fileManagerService.getAllMedia().subscribe(res => {
+      console.log(res);
+      this.allMedia = res;
+    });
+  }
+
+  getAllImages(): void {
+    this.fileManagerService.getAllImages().subscribe(res => {
+      console.log(res);
+      this.allImages = res;
+    });
+  }
+
+  getAllVideo(): void {
+    this.fileManagerService.getAllVideo().subscribe(res => {
+      console.log(res);
+      this.allVideo = res;
+    });
+  }
+
+  getAllAudio(): void {
+    this.fileManagerService.getAllAudio().subscribe(res => {
+      console.log(res);
+      this.allAudio = res;
+    });
   }
 
   hideFileManager(): void {
