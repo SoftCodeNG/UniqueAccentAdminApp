414b2ed34b0621ee2efaf4e13b26fdc5280cea8e
Uche Esther <ucheesther87@gmail.com>
2021-05-16 16:47:49 +0100

    API Integration

--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -63,4 +63,20 @@ export class CoursesService {
         })
       );
   }
+
+ createLesson(data: any): Observable<any> {
+    const payload = new FormData();
+    payload.append('title', data.title);
+    payload.append('duration', data.duration);
+    payload.append('courseId', data.courseId);
+    payload.append('thumbnail', data.thumbnail);
+    payload.append('description', data.description);
+    payload.append('video', data.video);
+    return this.http.post<any>(`${this.baseURL}courses/createLesson`, payload)
+     .pipe(
+        map(res => {
+          return res.payload;
+        })
+      );
+  }
 }
